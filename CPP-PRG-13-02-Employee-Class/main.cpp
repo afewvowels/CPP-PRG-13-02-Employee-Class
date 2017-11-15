38c9596f843af341c0e1f8e1afad095c8f4b6d8a
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-14 20:12:04 -0500

    Prompt written.

--- CPP-PRG-13-02-Employee-Class/main.cpp
+++ CPP-PRG-13-02-Employee-Class/main.cpp
@@ -5,11 +5,44 @@
 //  Created by Keith Smith on 11/14/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a class named Employee that has the following member variables:
+//
+//      * name - a string that holds the employee's name
+//      * idNumber - an int variable that holds the employee's ID number
+//      * department - a string that holds the name of the department where the employee
+//        works
+//      * position - a string that holds the employee's job title
+//
+//  The class should have the following constructors:
+//
+//      * A constructor that accepts the following values as arguments and assigns them to
+//        the appropriate member variables: employee's name, employee's ID number,
+//        department, and position.
+//      * A constructor that accepts the following values as arguments and assigns them to
+//        the appropriate member variables: employee's name and ID number. The department
+//        and position fields should be assigned an empty string ("").
+//      * A default constructor that assigns empty strings ("") to the name, department, and
+//        position member variables, and 0 to the idNumber member variable.
+//
+//  Write appropriate mutator functions that store values in these member variables and
+//  accessor functions that return the values in these member variables. Once you have
+//  written the class, write a separate program that creates three Employee objects to hold
+//  the following data:
+//
+//  Name                ID Number           Department              Position
+//  ------------------------------------------------------------------------------
+//  Susan Meyers        47899               Accounting              Vice President
+//  Mark Jones          39119               IT                      Programmer
+//  Joy Rogers          81774               Manufacturing           Engineer
+//
+//  The program should store this data in three objects and then display the data for
+//  each employee on the screen.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
